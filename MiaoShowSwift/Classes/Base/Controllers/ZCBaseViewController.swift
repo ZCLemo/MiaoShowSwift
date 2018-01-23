68dc5514014a2dda45ea771b8654ee1a32d80f44
赵琛 <18356033655@163.com>
2018-01-23 17:34:39 +0800

    first commit

---
+++ MiaoShowSwift/Classes/Base/Controllers/ZCBaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ZCBaseViewController.swift
+//  MiaoShowSwift
+//
+//  Created by 赵琛 on 2018/1/23.
+//  Copyright © 2018年 赵琛. All rights reserved.
+//
+
+import UIKit
+
+class ZCBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'MiaoShowSwift' do
+
+pod 'HandyJSON', '~> 4.0.0-beta.1'
+pod 'Alamofire'
+pod 'SnapKit'
+pod 'Moya'
+pod 'Kingfisher'
+pod 'MJRefresh'
+pod 'MBProgressHUD'
+pod 'IQKeyboardManagerSwift'
+pod 'MJRefresh'
+pod 'Cache'
+
+end
